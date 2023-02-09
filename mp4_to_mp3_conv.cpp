01bd38fc38e84e63035f66c535f01d4b666ac705
root <root@v1645578.hosted-by-vdsina.ru>
2023-02-09 14:52:32 +0300

    1.0 ver

---
+++ mp4_to_mp3_conv.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    cout << "Конвертация MP4 в MP3..." << endl;
+
+    // Путь к исходному файлу mp4 
+    string sourcePath = "C:\\source.mp4";
+
+    // Путь, куда будет сохранен mp3-файл 
+    string destPath = "C:\\dest.mp3";
+
+    // Команда ffmpeg, которая будет использоваться для конвертирования  
+    string command = "ffmpeg -i \"" + sourcePath + "\" \"" + destPath + "\"";
+
+    system(command.c_str());
+
+    cout << "Готово!";
+
+    return 0;
+}
